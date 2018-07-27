25f98108cd1fe413f5c75f4158b4268a61eaccb6
Anders Lau <webliupeng@gmail.com>
2018-07-27 18:01:34 +0800

    fix ci test

--- test.sh
+++ test.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
-set -e
-echo "" > coverage.txt
-
-for d in $(go list ./... | grep -v vendor); do
-    go test -race -coverprofile=profile.out -covermode=atomic $d
-    if [ -f profile.out ]; then
-        cat profile.out >> coverage.txt
-        rm profile.out
+echo "mode: set" > acc.out
+for Dir in $(find ./* -maxdepth 10 -type d ); 
+do
+	if ls $Dir/*.go &> /dev/null;
+	then
+		returnval=`go test -coverprofile=profile.out $Dir`
+		echo ${returnval}
+		if [[ ${returnval} != *FAIL* ]]
+		then
+    		if [ -f profile.out ]
+    		then
+        		cat profile.out | grep -v "mode: set" >> acc.out 
+    		fi
+    	else
+    		exit 1
+    	fi	
     fi
 done
+if [ -n "$COVERALLS" ]
+then
+	#goveralls -coverprofile=acc.out $COVERALLS
+    goveralls -coverprofile=coverage.txt -service travis-ci -repotoken $COVERALLS_TOKEN
+fi	
